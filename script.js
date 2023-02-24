83f36fa734c8ad25c861b0720b1e0c258e5f835e
Daniel Nguyen <daniel.nguye@gmail.com>
2023-02-24 09:40:27 -0500

    play and pause functionality

---
+++ script.js
@@ -0,0 +1,48 @@
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+
+// Song titles
+const songTitles = ['hey', 'summer', 'ukulele'];
+
+// Song tracker
+let songIndex = 1;
+
+// Initialize load song detaiks
+loadSong(songTitles[songIndex]);
+
+function loadSong(songTitle) {
+  title.innerText = songTitle;
+  audio.src = `music/${songTitle}.mp3`;
+  cover.src = `images/${songTitle}.jpg`;
+}
+
+function playSong() {
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
+  audio.play();
+}
+
+function pauseSong() {
+  musicContainer.classList.remove('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+  playBtn.querySelector('i.fas').classList.add('fa-play');
+  audio.pause();
+}
+
+playBtn.addEventListener('click', () => {
+  const isPlaying = musicContainer.classList.contains('play');
+
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
